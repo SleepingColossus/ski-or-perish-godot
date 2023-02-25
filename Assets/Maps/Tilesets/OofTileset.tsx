9757e2d645a5ebaa5dd027596ef545a841ba613e
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2023-02-25 10:58:55 +0000

    Expand special tileset with jump and end blocks

--- Assets/Maps/Tilesets/OofTileset.tsx
+++ Assets/Maps/Tilesets/OofTileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="OofTileset" tilewidth="64" tileheight="64" tilecount="1" columns="1">
- <image source="OofTileset.png" width="64" height="64"/>
+<tileset version="1.9" tiledversion="1.9.2" name="SpecialTileset" tilewidth="64" tileheight="64" tilecount="3" columns="3">
+ <image source="SpecialTileset.png" width="192" height="64"/>
 </tileset>
