ca1250488a57c2ec74ab605250cfb5be08c84f40
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2023-04-25 23:31:29 +0100

    Spawn new map segments when old segments despawn

--- Src/EndlessMode/EndlessMode.gd
+++ Src/EndlessMode/EndlessMode.gd
@@ -7,39 +7,49 @@ extends Node
 
 # from project setting: display/window/size/viewport_height
 const Y_OFFSET = 567
-var _current_segment : Map
+
+var game_velocity : float
 
 func _ready():
     $Player.vertical_velocity_changed.connect(_on_Player_vertical_velocity_changed)
 
     var initial_segment = center_segments[randi() % center_segments.size()]
     var map = initial_segment.instantiate()
-    _current_segment = map
+    map.map_destroyed.connect(_on_map_destroyed)
     maps_node.add_child(map)
 
-    _next_map()
+    _next_map(map.exit_point)
 
 
 func _process(_delta):
     pass
 
 
-func _on_Player_vertical_velocity_changed(new_velocity: float):
-    var maps = maps_node.get_children()
-    for map in maps:
-        map.set_map_velocity(new_velocity)
-
-func _next_map():
+func _next_map(exit_point: MapEnums.CrossingPoint):
     var next_segment : PackedScene
 
-    if _current_segment.exit_point == MapEnums.CrossingPoint.LEFT:
+    if exit_point == MapEnums.CrossingPoint.LEFT:
         next_segment = left_segments[randi() % left_segments.size()]
-    elif _current_segment.exit_point == MapEnums.CrossingPoint.RIGHT:
+    elif exit_point == MapEnums.CrossingPoint.RIGHT:
         next_segment = right_segments[randi() % right_segments.size()]
     else:
         next_segment = center_segments[randi() % center_segments.size()]
 
     var map = next_segment.instantiate()
-    _current_segment = map
+    map.set_map_velocity(game_velocity)
+    map.map_destroyed.connect(_on_map_destroyed)
     maps_node.add_child(map)
     map.position.y = Y_OFFSET
+
+
+func _on_Player_vertical_velocity_changed(new_velocity: float):
+    game_velocity = new_velocity
+
+    var maps = maps_node.get_children()
+    for map in maps:
+        map.set_map_velocity(new_velocity)
+
+
+func _on_map_destroyed(map: Map):
+    map.map_destroyed.disconnect(_on_map_destroyed)
+    _next_map(map.exit_point)
--- Src/Maps/Map.gd
+++ Src/Maps/Map.gd
@@ -2,9 +2,14 @@ class_name Map
 
 extends CharacterBody2D
 
+signal map_destroyed(map: Map)
+
 @export_group("CrossingPoint")
 @export var entry_point: MapEnums.CrossingPoint
 @export var exit_point: MapEnums.CrossingPoint
+
+# from project setting: display/window/size/viewport_height
+const DESTROY_OFFSET = 567
 
 func _ready():
     pass
@@ -12,6 +17,10 @@ func _ready():
 
 func _process(_delta):
     move_and_slide()
+
+    if position.y <= -DESTROY_OFFSET:
+        map_destroyed.emit(self)
+        queue_free()
 
 
 func set_map_velocity(new_velocity: float):
