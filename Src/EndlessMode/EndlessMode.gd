9926d60ef71852da0281b0b7bdcd8d86cab80f1f
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2023-04-25 22:59:13 +0100

    Randomly instantiate initial map segment

--- Src/EndlessMode/EndlessMode.gd
+++ Src/EndlessMode/EndlessMode.gd
@@ -1,16 +1,23 @@
 extends Node
 
-@onready var maps : Array[Node] = $Maps.get_children()
+@export var left_segments : Array[PackedScene]
+@export var center_segments : Array[PackedScene]
+@export var right_segments : Array[PackedScene]
+@onready var maps_node : Node = $Maps
 
 
 func _ready():
     $Player.vertical_velocity_changed.connect(_on_Player_vertical_velocity_changed)
 
+    var initial_segment = center_segments[randi() % center_segments.size()]
+    var map = initial_segment.instantiate()
+    maps_node.add_child(map)
 
-func _process(delta):
+func _process(_delta):
     pass
 
 
 func _on_Player_vertical_velocity_changed(new_velocity: float):
+    var maps = maps_node.get_children()
     for map in maps:
         map.set_map_velocity(new_velocity)
